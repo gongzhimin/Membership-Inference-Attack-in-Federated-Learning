315351f72fc3c0fc908501c316048c5661b4ce16
The-MinGo <3047673038@qq.com>
2020-12-10 10:57:30 +0800

    Update create_cifar100_train.py
    nurture

--- membership_inference_attack/datasets/create_cifar100_train.py
+++ membership_inference_attack/datasets/create_cifar100_train.py
@@ -1,30 +1,30 @@
-import cPickle
+import pickle
 import numpy as np
 import os
 
 def unpickle(file):
     with open(file, 'rb') as fo:
-        dict = cPickle.load(fo)
+        dict = pickle.load(fo, encoding="bytes")
     return dict
 
 traindict = unpickle("./cifar-100-python/train")
 testdict  = unpickle("./cifar-100-python/test")
 
 with open("c100.txt.tmp", "w+") as f:
-	for i in xrange(len(traindict['data'])):	
-		a = ','.join([str(c) for c in traindict['data'][i][:1024]]) + ';' + \
-			','.join([str(c) for c in traindict['data'][i][1024:2048]]) + ';' + \
-			','.join([str(c) for c in traindict['data'][i][2048:]]) + ';' + \
-			str(traindict['fine_labels'][i])
-		f.write(a+"\n")
+    for i in range(len(traindict[b'data'])):
+        a = ','.join([str(c) for c in traindict[b'data'][i][:1024]]) + ';' + \
+            ','.join([str(c) for c in traindict[b'data'][i][1024:2048]]) + ';' + \
+            ','.join([str(c) for c in traindict[b'data'][i][2048:]]) + ';' + \
+            str(traindict[b'fine_labels'][i])
+        f.write(a+"\n")
 
 def extract(filepath):
     """
     """
     with open(filepath, "r") as f:
         dataset = f.readlines()
-    dataset = map(lambda i: i.strip('\n').decode("utf-8").split(';'), dataset)
-    dataset = np.array(list(dataset)) 
+    dataset = map(lambda i: i.strip('\n').encode('utf-8').decode("utf-8").split(';'), dataset)
+    dataset = np.array(list(dataset))
     return dataset
 
 dataset = extract('c100.txt.tmp')
