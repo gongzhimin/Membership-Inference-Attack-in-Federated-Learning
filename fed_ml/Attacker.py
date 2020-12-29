a0c3ecfcb7cef4a649b2373651689e5816c1fcf1
The-MinGo <3047673038@qq.com>
2020-12-29 10:15:16 +0800

    lift the ban on gradient ascent attack and fix a issue

--- fed_ml/Attacker.py
+++ fed_ml/Attacker.py
@@ -26,12 +26,13 @@ class Attacker:
                                                                              attack_percentage=attack_percentage,
                                                                              input_shape=client.input_shape)
 
-    def generate_target_gradient(self, client, instances_num=5):    # 100， 10， 50， 5
+    def generate_target_gradient(self, client, instances_num=3):    # 100， 10， 50， 5, 1, 3
         self.target_member_features = self.data_handler.exposed_features[:instances_num]
         self.target_member_labels = self.data_handler.exposed_labels[:instances_num]
         with tf.GradientTape() as tape:
             logits = client.model(self.target_member_features)
             loss = CrossEntropyLoss(logits, self.target_member_labels)
+            loss = tf.reduce_mean(loss)
         target_var = client.model.trainable_variables
         self.target_gradients = copy.deepcopy(tape.gradient(loss, target_var))
 
---
+++ global_gradient_ascent_attack_fed.py
@@ -0,0 +1,59 @@
+from fed_ml.Client import Clients
+from fed_ml.Server import Server
+from fed_ml.Attacker import Attacker
+
+
+if __name__ == "__main__":
+    """Set hyper-parameters."""
+    epoch = 6
+    learning_rate = 0.0001
+    # The ml_privacy_meter can't handle the scenario with too many participants.
+    CLIENT_NUMBER = 4
+    # And as federated learning is online,
+    # participants are uncertain about their online status in each training epoch.
+    CLIENT_RATIO_PER_ROUND = 1.00
+    # Some characteristics of the dataset cifar-100.
+    input_shape = (32, 32, 3)
+    # classes_num = 100   # cifar-100
+    classes_num = 10    # cifar-10
+
+    """Build clients, server and attacker."""
+    client = Clients(input_shape=input_shape,
+                    classes_num=classes_num,
+                    learning_rate=learning_rate,
+                    clients_num=CLIENT_NUMBER,
+                     dataset_path="./datasets/cifar100.txt")
+    server = Server()
+    attacker = Attacker()
+
+    """Target the attack."""
+    target_cid = 1
+    target_ep = 5
+    attacker.declare_attack("GGAA", target_cid, target_ep)
+    attacker.generate_attack_data(client)
+
+    """Begin training."""
+    for ep in range(epoch):
+        # Empty local_parameters_sum at the beginning of each epoch.
+        server.initialize_local_parameters_sum()
+        # Choose a random selection of active clients to train in this epoch.
+        active_clients = client.choose_clients(CLIENT_RATIO_PER_ROUND)
+        # Train these clients.
+        for client_id in active_clients:
+            client.current_cid = client_id
+            print("[fed-epoch {}] cid: {}".format(ep, client_id))
+            if ep == target_ep and client_id == target_cid:
+                client.download_global_parameters(server.global_parameters)
+                attacker.generate_target_gradient(client)
+                attacker.craft_global_parameters(server.global_parameters)
+                print("the global parameters have been crafted.")
+            client.download_global_parameters(server.global_parameters)
+            client.train_local_model()
+            # Accumulate local parameters.
+            current_local_parameters = client.upload_local_parameters()
+            server.accumulate_local_parameters(current_local_parameters)
+            if ep == target_ep and client_id == target_cid:
+                print("global gradient ascent attack on cid: {} in fed-epoch: {}".format(client_id, ep))
+                attacker.membership_inference_attack(client)
+        # Update global parameters in each epoch.
+        server.update_global_parameters(len(active_clients))

lift the ban on gradient ascent attack and fix a issue

fed_ml/Attacker.py
@@ -26,12 +26,13 @@ class Attacker:
                                                                              attack_percentage=attack_percentage,
                                                                              input_shape=client.input_shape)
 
-    def generate_target_gradient(self, client, instances_num=5):    # 100， 10， 50， 5
+    def generate_target_gradient(self, client, instances_num=3):    # 100， 10， 50， 5, 1, 3
         self.target_member_features = self.data_handler.exposed_features[:instances_num]
         self.target_member_labels = self.data_handler.exposed_labels[:instances_num]
         with tf.GradientTape() as tape:
             logits = client.model(self.target_member_features)
             loss = CrossEntropyLoss(logits, self.target_member_labels)
+            loss = tf.reduce_mean(loss)
         target_var = client.model.trainable_variables
         self.target_gradients = copy.deepcopy(tape.gradient(loss, target_var))
 